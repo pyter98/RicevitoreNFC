f1cb7da23b1b81e66daf88e2bd003854e0dd536c
Piero <pyter9@gmail.com>
2019-11-24 18:19:59 +0100

    4

--- app/src/main/java/com/example/ricevitorenfc/MainActivity.java
+++ app/src/main/java/com/example/ricevitorenfc/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ricevitorenfc;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -11,37 +15,62 @@ import android.nfc.tech.MifareUltralight;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
     private NfcAdapter nfcAdapter;
     private PendingIntent pendingIntent;
-    private TextView text;
-    private Button button;
+    private TextView text, initext;
+    private Button  verified;
+    private Timer timer;
+    private  static String URL_READ = "https://nearfit.altervista.org/fitness2/check.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.text);
+        initext = findViewById(R.id.text1);
+        verified = findViewById(R.id.verified);
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        button = findViewById(R.id.NewScan);
+        /*button = findViewById(R.id.NewScan);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
+                MainActivity.this.finish();
+                initext.setVisibility(View.VISIBLE);
             }
-        });
+        });*/
         if (nfcAdapter == null) {
             Toast.makeText(this, "No NFC", Toast.LENGTH_SHORT).show();
             finish();
@@ -112,30 +141,112 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @SuppressLint("SetTextI18n")
     private void displayMsgs(NdefMessage[] msgs) {
         if (msgs == null || msgs.length == 0)
             return;
 
         StringBuilder builder = new StringBuilder();
         List<ParsedNdefRecord> records = NdefMessageParser.parse(msgs[0]);
-        final int size = records.size();
 
-        /*for (int i = 0; i < size; i++) {
-            ParsedNdefRecord record = records.get(i);
-            String str = record.str();
-            builder.append(str).append("\n");
-        }*/
-
+        //Acquisisco username e password se presenti
         ParsedNdefRecord record = records.get(0);
         String str = record.str();
         builder.append(str);
 
-        String [] credenziali= builder.toString().split(" ");
-        String username = credenziali[0];
-        String password = credenziali[1];
+        //Verifico tutti i casi possibili
+        if (builder.toString().isEmpty()){
+            text.setText("Accesso negato");
+            initext.setVisibility(View.GONE);
+            verified.setVisibility(View.VISIBLE);
+            refresh();
+        }
+        else {
+            String [] credenziali= builder.toString().split(" ");
+            if (credenziali.length == 2) {
+                String username = credenziali[0];
+                String password = credenziali[1];
 
-        text.setText("Username: "+username + "\nPassword: " + password);
-        button.setVisibility(View.VISIBLE);
+                checkDB(username, password);
+
+            }
+            else {
+                text.setText("Accesso negato");
+                initext.setVisibility(View.GONE);
+                verified.setVisibility(View.VISIBLE);
+                refresh();
+
+            }
+        }
+
+        //text.setText("Username: "+username + "\nPassword: " + password);
+        //button.setVisibility(View.VISIBLE);
     }
+
+    private void checkDB(final String username, final String password){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_READ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            if (success.equals("1")) {
+                                text.setText("Benvenuto in palestra "+username);
+                                initext.setVisibility(View.GONE);
+                                verified.setVisibility(View.VISIBLE);
+                                verified.setBackgroundResource(R.drawable.circle_tre);
+                                refresh();
+                            }
+                            else {
+                                text.setText("Accesso Negato");
+                                initext.setVisibility(View.GONE);
+                                verified.setVisibility(View.VISIBLE);
+                                refresh();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Errore "+e.toString(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Errore "+error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                })
+        {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<>();
+                    params.put("user_name", username);
+                    params.put("password", password);
+                    return params;
+                }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
+    }
+
+    public void refresh()
+    {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+        },5000);
+    }
+
 
 }
